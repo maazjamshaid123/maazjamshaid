5a844fcae9e1d6dd8d2c57bfe854c5712a57db2a
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2024-06-21 22:49:11 +0500

    Update projects.py

--- projects.py
+++ projects.py
@@ -11,7 +11,7 @@ def show_projects():
     col2.caption('''Utilizes computer vision and Kalman filtering techniques to track objects in real-time. The application 
                  allows users to select an object for tracking by clicking on it in the video stream, and then visualizes the 
                  tracking process, including predicted object positions and velocities.''')
-    col2.text("[View More](https://github.com/maazjamshaid123/Target-Tracker-using-OpenCV-and-Kalman-Filter)")
+    col2.caption("[View More](https://github.com/maazjamshaid123/Target-Tracker-using-OpenCV-and-Kalman-Filter)")
 
     # AI Research Assistant
     col1, col2 = st.columns(2)
@@ -20,8 +20,8 @@ def show_projects():
     col2.title("AI Research Assistant for Space Scientists")
     col2.caption('''Leverages data-driven approaches such as data analysis, visualization, machine learning, and artificial 
                intelligence using OpenAI and provides effective solutions for a wide range of problems.''')
-    col2.text("[Explore](https://odyssey.streamlit.app/)")
-    col2.text("[View More](https://github.com/maazjamshaid123/AI-Research-Assistant-Odyssey)")
+    col2.caption("[Explore](https://odyssey.streamlit.app/)")
+    col2.caption("[View More](https://github.com/maazjamshaid123/AI-Research-Assistant-Odyssey)")
 
 
     
