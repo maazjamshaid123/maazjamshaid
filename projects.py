893352248ee5cda557171455006f8566809f1851
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2024-10-08 10:22:46 +0500

    Update projects.py

--- projects.py
+++ projects.py
@@ -6,6 +6,18 @@ def show_projects():
     col7.markdown("[![Github](https://cdn0.iconfinder.com/data/icons/social-line-transparent/50/Github-line-transparent-128.png)](https://github.com/maazjamshaid123)")
     st.markdown("---")
 
+    #SIYI APP
+    col1, col2 = st.columns(2)
+    image_url = "images/a8.png"
+    col1.image(image_url, width = 400)
+    col2.title("SIYI Gimbal Control System App")
+    col2.caption('''This gimbal control app is designed for SIYI Gimbals, including models like the A8 Mini and ZR30. 
+                    It allows you to manage various gimbal functions, such as movement, zoom, and codec settings.
+                    Additionally, the app enables you to test different hex commands to execute specific operations.''')
+    col2.caption("[`View More`](https://github.com/maazjamshaid123/SIYI-Gimbal-Control-App)")
+    
+    st.markdown("---")
+    
     # UAV Object Tracker
     col1, col2 = st.columns(2)
     image_url = "images//tgt_mil.png"
@@ -42,12 +54,4 @@ def show_projects():
     col2.caption("[`Explore`](https://odyssey.streamlit.app/)")
     col2.caption("[`View More`](https://github.com/maazjamshaid123/AI-Research-Assistant-Odyssey)")
 
-    #SIYI APP
-    col1, col2 = st.columns(2)
-    image_url = "images/a8.png"
-    col1.image(image_url, width = 400)
-    col2.title("SIYI Gimbal Control System App")
-    col2.caption('''This gimbal control app is designed for SIYI Gimbals, including models like the A8 Mini and ZR30. 
-                    It allows you to manage various gimbal functions, such as movement, zoom, and codec settings.
-                    Additionally, the app enables you to test different hex commands to execute specific operations.''')
-    col2.caption("[`View More`](https://github.com/maazjamshaid123/SIYI-Gimbal-Control-App)")
+    st.markdown("---")
